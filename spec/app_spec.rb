b8daaddcdeb4f3ac542c3254c94d11f4b244f88a
Richard <r.richard0000@gmail.com>
2020-07-05 13:20:15 -0300

    Tasks scheduler added. More tests about decoder for next iteration.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,10 +8,28 @@ describe 'Resources Service' do
     expect(last_response).to match('Welcome!')
   end
 
-  it 'shoul list all the resources at /api/v1/resources' do
+  it 'Should list all the resources at /api/v1/resources' do
     get '/api/v1/resources'
     expect(last_response).to be_ok
     # String taken from the (CR resources public api endpoint)[https://staging.credentialengineregistry.org/envelopes]
     expect(last_response.body).to include('envelope_community')
+  end
+end
+
+# Tests for the Decoder layer. This is not included in this 8 hours work,
+# but useful to have it as a reminder of what's next
+describe 'Decoder' do
+  it 'Should correctly decode a resource' do
+  end
+
+  it 'The JWT resource field was encoded by the public key provided in the envelope' do
+  end
+end
+
+# Tests for a single resource (when it's already in the DB -not scoped 
+# in this 8 hours work, but useful to have as a 'must include' test)
+describe 'Resource' do
+  # Validate the most important field inside the JWT (when it's decoded)
+  it 'Should contain a <field> attribute' do
   end
 end
